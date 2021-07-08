6fe5f1724f4a34c926af0fb48109d40bab6892c7
Andrea <rakosfalvyandrea@gmail.com>
2021-07-08 12:21:27 +0200

    onClick function added

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import './button.css';
 
+let timeOut;
+let prevHTML;
+
+function clickHappened(e) {
+
+    console.log(prevHTML)
+    
+    const goodHTML = "Goodbye";
+    
+    if (e.target.innerHTML !== goodHTML) {
+        prevHTML = e.target.innerHTML;
+        e.target.innerHTML = goodHTML;
+        timeOut = setTimeout(()=>e.target.innerHTML = prevHTML, 3000);
+    } else {
+        clearTimeout(timeOut);
+        e.target.innerHTML = prevHTML;
+    }
+
+}
+
 function Button(props) {
 
   
     return (
-        <button>{props.text}</button>
+        <button onClick={clickHappened}>{props.text}</button>
     );
 }
 
