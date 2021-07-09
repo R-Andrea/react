cf7ecda894343d48d427dd4357096da97a0e4413
Andrea <rakosfalvyandrea@gmail.com>
2021-07-09 10:42:36 +0200

    list of buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 import Button from "./components/button/button";
 
+
+
 function App() {
+
+  const songs = ["The Price of a Mile", "Gott mit uns", "Primo Victoria", "Great War", "The Red Baron"];
+
+  //Return songs as an unordered list
+  // function List (props) {
+  //   const list = songs.map((song)=> <li>{song}</li>);
+  //   return (list)
+  // }
+
+  function List () {
+    const listButtons = songs.map((song)=> <Button text={song}></Button>)
+    return (listButtons)
+  }
+
+ 
   return (
     <>
       <div className="form">
@@ -10,6 +27,7 @@ function App() {
       <div className="form2">
       <Button text="Send 2"/>
       </div>
+      <ul><List></List></ul>
     </>
   );
 }
